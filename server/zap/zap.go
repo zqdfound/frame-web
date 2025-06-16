07ebb221af5dd1378410a520cd4f9af50b39303e
zhuangqingdian <15950515069@163.com>
2025-06-16 17:41:01 +0800

    日志封装

--- server/zap/zap.go
+++ server/zap/zap.go
@@ -36,34 +36,48 @@ func getWriterSyncer() zapcore.WriteSyncer {
 }
 
 func Info(msg string, args ...interface{}) {
-	fields := make([]zap.Field, 0, len(args))
-	for _, arg := range args {
-		switch v := arg.(type) {
+	fields := make([]zap.Field, 0, len(args)/2)
+	for i := 0; i < len(args); i += 2 {
+		if i+1 >= len(args) {
+			break
+		}
+		key, ok := args[i].(string)
+		if !ok {
+			continue
+		}
+		switch v := args[i+1].(type) {
 		case string:
-			fields = append(fields, zap.String("value", v))
+			fields = append(fields, zap.String(key, v))
 		case int:
-			fields = append(fields, zap.Int("value", v))
+			fields = append(fields, zap.Int(key, v))
 		case error:
 			fields = append(fields, zap.Error(v))
 		default:
-			fields = append(fields, zap.Any("value", v))
+			fields = append(fields, zap.Any(key, v))
 		}
 	}
 	zap.L().Info(msg, fields...)
 }
 
 func Error(msg string, args ...interface{}) {
-	fields := make([]zap.Field, 0, len(args))
-	for _, arg := range args {
-		switch v := arg.(type) {
+	fields := make([]zap.Field, 0, len(args)/2)
+	for i := 0; i < len(args); i += 2 {
+		if i+1 >= len(args) {
+			break
+		}
+		key, ok := args[i].(string)
+		if !ok {
+			continue
+		}
+		switch v := args[i+1].(type) {
 		case string:
-			fields = append(fields, zap.String("value", v))
+			fields = append(fields, zap.String(key, v))
 		case int:
-			fields = append(fields, zap.Int("value", v))
+			fields = append(fields, zap.Int(key, v))
 		case error:
 			fields = append(fields, zap.Error(v))
 		default:
-			fields = append(fields, zap.Any("value", v))
+			fields = append(fields, zap.Any(key, v))
 		}
 	}
 	zap.L().Error(msg, fields...)
