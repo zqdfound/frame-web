fbff965a68e52d6f04cab8f3622ea0d816e12d68
zhuangqingdian <15950515069@163.com>
2025-06-26 16:36:20 +0800

    delete oss file

--- server/api/file_upload.go
+++ server/api/file_upload.go
@@ -1,9 +1,12 @@
 package api
 
 import (
+	"frame-web/global"
 	"frame-web/model/response"
 	"frame-web/svc/thirdService"
+
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 )
 
 func UploadFile(c *gin.Context) {
@@ -17,6 +20,7 @@ func UploadFile(c *gin.Context) {
 		response.FailWithMessage("上传文件失败:"+uploadErr.Error(), c)
 		return
 	}
+	global.LOG.Info("文件上传成功", zap.String("url", url), zap.String("relativePath", relativePath))
 	response.OkWithData(map[string]string{
 		"url":          url,
 		"relativePath": relativePath,
@@ -34,5 +38,6 @@ func DeleteFile(c *gin.Context) {
 		response.FailWithMessage("删除文件失败:"+err.Error(), c)
 		return
 	}
+	global.LOG.Info("文件删除成功", zap.String("relativePath", relativePath))
 	response.OkWithMessage("删除成功", c)
 }
--- server/svc/models/User.go
+++ server/svc/models/User.go
@@ -2,12 +2,16 @@ package models
 
 import (
 	"frame-web/model"
+
+	"github.com/go-playground/validator/v10"
 )
+
+var validate = validator.New()
 
 type User struct {
 	model.COMMON_MODEL
 	//ID        uint      `gorm:"primaryKey;column:id;comment:主键ID" json:"id,omitempty"`
-	Username string `gorm:"size:255;column:username;not null;uniqueIndex;comment:用户名" json:"username"`
+	Username string `gorm:"size:255;column:username;not null;uniqueIndex;comment:用户名" json:"username"  binding:"required"`
 	Password string `gorm:"size:255;column:password;not null;comment:密码" json:"-"`
 	Email    string `gorm:"size:255;column:email;comment:邮箱" json:"email,omitempty"`
 	Status   int    `gorm:"type:tinyint;column:status;default:1;comment:状态(1:正常 0:禁用)" json:"status,omitempty"`
